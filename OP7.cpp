d8940aacf59c91196c38af838e07fb8c4a434f42
mvertoholova <margaritkaritka03@gmail.com>
2023-10-26 08:39:21 +0300

    New code

---
+++ OP7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <windows.h>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "----\tДавай розв'яжемо це завдання\t----\n\n";
+
+    float Y, sumchik, x, i;
+    cout << "Введіть x=\n";
+    cin >> x;
+    sumchik = 1;
+
+    if (pow(tan(x), 2) == 0) {
+        cout << "ERROR :(";
+    }
+    else {
+        for (int n = 1; n < 6; n++) {
+            sumchik += pow(n, 3);
+        }
+        Y = cos(sumchik) + log(x) / (pow(tan(x), 2));
+        cout << "---\tВідповідь\t---\n" << "Y= " << Y;
+    }
+}
