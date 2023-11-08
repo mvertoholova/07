9c02f281d7629e4c51ae390bfbe707462244da21
mvertoholova <margaritkaritka03@gmail.com>
2023-11-08 21:47:30 +0200

    Зміна кода на лістинг 2

--- OP7.cpp
+++ OP7.cpp
@@ -8,21 +8,17 @@ int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    cout << "----\t!!Розв'яжемо це завдання!!\t----\n\n";
+    cout << "----\tДавай розв'яжемо це завдання\t----\n\n";
 
-    float y, sumchik, x, i;
+    float Z, dobchik, x;
     cout << "Введіть x=\n";
     cin >> x;
-    sumchik = 1;
+    dobchik = 1;
 
-    if (pow(tan(x), 2) == 0) {
-        cout << "ERROR :(";
+    for (int i = 1; i <= 10; i++) {
+        dobchik *= log(i * x);
     }
-    else {
-        for (int n = 1; n < 6; n++) {
-            sumchik += pow(n, 3);
-        }
-        Y = cos(sumchik) + log(x) / (pow(tan(x), 2));
-        cout << "---\tВідповідь\t---\n" << "Y= " << y;
-    }
+    Z = sin(dobchik);
+    cout << "---\tВідповідь\t---\n" << "Z= " << Z;
 }
+
